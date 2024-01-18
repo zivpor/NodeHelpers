e32aaa7d2b5891af66728799440952699f9b9dc4
User <User@TEACHER3>
2024-01-18 13:50:35 +0200

    Update Shabat

--- NodeInserts/Models/ShabatRecievers.cs
+++ NodeInserts/Models/ShabatRecievers.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace NodeInserts.Models
 {
-    internal class ShabatRecievers
+    public  class ShabatRecievers
     {
+        private string parent1;
+        private string parent2;
+        private int day;
+        private int month;
+        private int year;   
+
+        public ShabatRecievers(string name1,string name2, int day, int month, int year) 
+        {
+            this.parent1 = name1;
+            this.parent2 = name2;
+            this.month = month;    
+            this.year = year;
+            this.day=day;
+
+        }
+
+        public string GetParent1()
+        { return parent1; }
+        public string GetParent2() { return parent2; }
+        public int GetDay() { return day; }
+        public int GetMonth() { return month; }
+        public int GetYear() { return year; }
+
+
     }
 }
