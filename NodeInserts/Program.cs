d7cca89cc4871ca8b424af16c9d9a22a1025edf5
User <User@TEACHER3>
2024-01-18 13:48:22 +0200

    Update ShabatShlom

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -1,12 +1,20 @@
 ﻿using NodeClass;
+using NodeInserts.Models;
+
 namespace NodeInserts
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Node<int> lst = new Node<int>(4, new Node<int>(1, new Node<int>(1, new Node<int>(1))));
-            Console.WriteLine(NodeHelper.CountNoOfDuplicationV2(lst,4));
+            Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
+            Node<ShabatRecievers> nextShabat = new Node<ShabatRecievers>(new ShabatRecievers("Ofek", "Aviv", 25, 1, 2024));
+            shabatShalom.SetNext(nextShabat);
+
+            ShabatRecievers sh = shabatShalom.GetValue();
+            string p1 = sh.GetParent1();
+
+
         }
     }
 }
