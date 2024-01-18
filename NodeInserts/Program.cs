93754653bbd62b5faabee58d055419757af504b4
User <User@LAB3-3>
2024-01-18 14:39:20 +0200

    rhgerh

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -5,6 +5,33 @@ namespace NodeInserts
 {
     internal class Program
     {
+        public static bool IsShabatExist(Node<ShabatRecievers> shabat ,int day, int month, int year)
+        {
+            while(shabat != null)
+            {
+                if(shabat.GetValue().GetDay() == day&&shabat.GetValue().GetMonth()==month&&shabat.GetValue().GetYear()==year)
+                {
+                    return true;
+                }
+                shabat=shabat.GetNext();
+            }
+            return false;
+        }
+        public static Node<ShabatRecievers> DeleteSick(Node<ShabatRecievers> shabat,string name)
+        {
+            Node<ShabatRecievers> head = shabat;
+            if(shabat.GetValue().GetParent1()==name||shabat.GetValue().GetParent2()==name)
+            {
+                head = head.GetNext();
+                shabat.SetNext(null);
+                return head;
+            }
+            while(shabat != null)
+            {
+                //עשיתי אצל שירה!!!!
+            }
+            return head;
+        }
         static void Main(string[] args)
         {
             Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
